7811cff9fc4ad3ebdebdd8e9fd4c8ff379e6a193
Michael Brown <mbrown@fensystems.co.uk>
2018-10-12 00:45:26 +0100

    [REL] Release version 1.1
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

--- addons/print/__manifest__.py
+++ addons/print/__manifest__.py
@@ -15,7 +15,7 @@ Key Features
 * Define a default printer for each user
 * Define a system default printer
     """,
-    'version': '1.0',
+    'version': '1.1',
     'author': 'Michael Brown <mbrown@fensystems.co.uk>',
     'category': 'Extra Tools',
     'depends': [
