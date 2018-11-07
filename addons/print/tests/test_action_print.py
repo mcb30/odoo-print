e432461efddcdef2eb8e9ac17f0c8fcc2e4d77e9
David Spence <david.spence@unipart.io>
2018-11-07 22:12:16 +0000

    [IMP] odoo-print: derived print strategy testing
    Support execution of automated printing tests for derived models
    providing specialised print strategies.
    
    User-story: 1522
    Task: 2083
    Signed-off-by: David Spence <david.spence@unipart.io>

--- addons/print/tests/test_action_print.py
+++ addons/print/tests/test_action_print.py
@@ -10,6 +10,7 @@ from .common import PrinterCase
 class TestActionPrint(PrinterCase):
     """Printing action tests"""
     action_model = 'ir.actions.server'
+    strategy_model = 'print.strategy'
 
     @property
     def default_report(self):
@@ -26,24 +27,41 @@ class TestActionPrint(PrinterCase):
         """Return the model id of `model`."""
         return cls.env['ir.model']._get_id(model)
 
-    def create_action(self, name, model='print.strategy'):
-        """Return new print action with `name` and print strategy `model`."""
+    def create_action(self, name, model=True):
+        """Return a new print action with `name` and print strategy `model`.
+           If `model` is True, then use :attr:`strategy_model` instead.
+        """
+        if model is True:
+            model = self.strategy_model
         return self.env[self.action_model].create({
             'name': name,
             'model_id': self.model_id(self.action_model),
             'state': 'print',
-            'strategy_id': self.model_id(model),
+            'strategy_id': None if model is None else self.model_id(model),
         })
 
-    def create_strategy(self, name, report, printer, safety=None,
-                        model='print.strategy'):
-        """Return new print strategy `model` instance from given args."""
-        return self.env[model].create({
+    def create_strategy(self, name, report, printer,
+                        safety=None, model=True, **kwargs):
+        """Return a new print strategy instance from kwargs overridden by args.
+           If `model` is True, then use :attr:`strategy_model` instead.
+        """
+        if model is True:
+            model = self.strategy_model
+        kwargs.update({
             'name': name,
             'report_id': None if report is None else report.id,
             'printer_id': None if printer is None else printer.id,
             'safety': safety,
         })
+        return self.env[model].create(kwargs)
+
+    def action_context(self, obj):
+        """Return an action run context which may select strategies."""
+        return {'active_model': obj._name, 'active_ids': obj.ids}
+
+    def benign_context(self, obj):
+        """Return an action run context which selects no strategies."""
+        return self.action_context(obj)
 
     def test01_missing_strategy(self):
         """Test print action must specify strategy_id"""
@@ -53,12 +71,8 @@ class TestActionPrint(PrinterCase):
     def test02_no_strategies(self):
         """Test print action with no strategies"""
         action = self.create_action('no strategies')
-        # nothing to be printed when the action is run
-        # action may be run in any context: context object is benign
-        action.with_context(
-            active_model=action._name,
-            active_ids=action.ids,
-        ).run()
+        # nothing to be printed when the action is run in the wrong context
+        action.with_context(**self.benign_context(action)).run()
         self.mock_subprocess.Popen.assert_not_called()
 
     def test03_missing_report(self):
@@ -75,17 +89,11 @@ class TestActionPrint(PrinterCase):
         action = self.create_action('one strategy')
         self.create_strategy('Chesney', self.default_report, None)
         # nothing to be printed when the action is run in the wrong context
-        action.with_context(
-            active_model=action._name,
-            active_ids=action.ids,
-        ).run()
+        action.with_context(**self.benign_context(action)).run()
         self.mock_subprocess.Popen.assert_not_called()
         # a single document to be printed when run in the right context
         printer = self.default_printer
-        action.with_context(
-            active_model=printer._name,
-            active_ids=printer.ids,
-        ).run()
+        action.with_context(**self.action_context(printer)).run()
         self.assertPrintedLpr('-T', ANY)
 
     def test06_two_strategies(self):
@@ -94,17 +102,11 @@ class TestActionPrint(PrinterCase):
         self.create_strategy('Ant', self.default_report, None)
         self.create_strategy('Dec', self.default_report, None)
         # nothing to be printed when the action is run in the wrong context
-        action.with_context(
-            active_model=action._name,
-            active_ids=action.ids,
-        ).run()
+        action.with_context(**self.benign_context(action)).run()
         self.mock_subprocess.Popen.assert_not_called()
         # two documents to be printed when run in the right context
         printer = self.default_printer
-        action.with_context(
-            active_model=printer._name,
-            active_ids=printer.ids,
-        ).run()
+        action.with_context(**self.action_context(printer)).run()
         self.assertPrintedLprMulti(
             ('-T', ANY),
             ('-T', ANY),
@@ -116,8 +118,5 @@ class TestActionPrint(PrinterCase):
         self.create_strategy('Test 07', self.default_report, None, 'test07')
         # nothing to be printed when the safety catch is not disabled
         printer = self.default_printer
-        action.with_context(
-            active_model=printer._name,
-            active_ids=printer.ids,
-        ).run()
+        action.with_context(**self.action_context(printer)).run()
         self.mock_subprocess.Popen.assert_not_called()
